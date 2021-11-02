2f1b7a4cfe9846b5ceed741b0b025db9102dc0c0
Deep Hansda <deephansda921@gmail.com>
2021-11-02 15:29:14 +0530

    modified cors

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-app.use(cors())
+app.use(cors({
+    origin: '*',
+    methods: ['GET']
+}));
 require('./database/connect/mongo_connection');
 const tourRouter = require('./router/tour_router')
 const port = process.env.PORT || 5000;
